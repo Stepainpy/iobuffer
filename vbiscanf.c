580c7327bc60317ac34bf80e376f18091b160021
Stepainpy <stepa2014vlg@gmail.com>
2026-05-26 15:38:59 +0300

    add generic integer specifier

--- vbiscanf.c
+++ vbiscanf.c
@@ -31,8 +31,15 @@ typedef uint64_t scanset_t[4];
 static void bissset(scanset_t ss, uchar value) { ss[value / 64] |= UINT64_C(1) << (value % 64); }
 static bool bissget(scanset_t ss, uchar value) { return (ss[value / 64] >> (value % 64)) & 1; }
 
-static bool biisspace(char ch) {
+static bool biisspace(int ch) {
     return memchr(B_SPACE_CHARS, ch, sizeof B_SPACE_CHARS - 1) != NULL;
+}
+
+static int bichartodigit(int ch) {
+    if ('0' <= ch && ch <= '9') return ch - '0'     ;
+    if ('A' <= ch && ch <= 'Z') return ch - 'A' + 10;
+    if ('a' <= ch && ch <= 'z') return ch - 'a' + 10;
+    return -1;
 }
 
 static int bistrtoint(const char* str, const char** end) {
@@ -74,6 +81,99 @@ static const char* biparsescanlist(scanset_t ss, bool* inverse, const char* fmts
     if (*fmtstr == '\0') return NULL;
 
     return fmtstr;
+}
+
+static int bistrtouim(BUFFER* buf, bifmtspec_t* fmt, va_list args, int base, int* total, bool signing) {
+    uintmax_t result;
+    bool zero_base = false;
+    bool is_neg = false;
+    int ch, digit;
+
+    while (biisspace(biimmpeek(buf)))
+        biimmskip(buf), ++*total;
+
+    if (fmt->maxwidth > 0) {
+        ch = biimmpeek(buf);
+        /*  */ if (ch == '-') {
+            biimmskip(buf), --fmt->maxwidth, ++*total;
+            is_neg = true;
+        } else if (ch == '+')
+            biimmskip(buf), --fmt->maxwidth, ++*total;
+    }
+
+    if (base == 0) {
+        if (fmt->maxwidth > 0 && biimmpeek(buf) == '0') {
+            biimmskip(buf), --fmt->maxwidth, ++*total;
+
+            zero_base = true;
+            base = 8;
+
+            if (fmt->maxwidth > 0) {
+                ch = biimmpeek(buf);
+                if (ch == 'x' || ch == 'X') {
+                    biimmskip(buf), --fmt->maxwidth, ++*total;
+                    base = 16;
+                }
+            }
+        } else
+            base = 10;
+    }
+
+    if (fmt->maxwidth > 0) {
+        ch = biimmpeek(buf);
+        if (ch == EOB) goto prefix_only;
+
+        digit = bichartodigit(ch);
+        if (digit < 0 || digit >= base) goto prefix_only;
+
+        biimmskip(buf), --fmt->maxwidth, ++*total;
+        result = digit;
+
+        while (fmt->maxwidth > 0 && (ch = biimmpeek(buf)) != EOB) {
+            digit = bichartodigit(ch);
+            if (digit < 0 || digit >= base) break;
+            biimmskip(buf), --fmt->maxwidth, ++*total;
+
+            result = result * base + digit;
+        }
+    } else
+        goto prefix_only;
+
+    goto assigning;
+
+prefix_only:
+    if (!zero_base) return B_FAIL;
+    result = 0;
+
+assigning:
+    if (is_neg) result = -result;
+
+    /**/ if (fmt->assign &&  signing)
+        switch (fmt->lenmod) {
+            case BLM_NONE: *va_arg(args,       int*) = result; break;
+            case BLM_HH  : *va_arg(args,     schar*) = result; break;
+            case BLM_H   : *va_arg(args,     short*) = result; break;
+            case BLM_L   : *va_arg(args,      long*) = result; break;
+            case BLM_LL  : *va_arg(args,    sllong*) = result; break;
+            case BLM_J   : *va_arg(args,  intmax_t*) = result; break;
+            case BLM_Z   : *va_arg(args,    size_t*) = result; break;
+            case BLM_T   : *va_arg(args, ptrdiff_t*) = result; break;
+            case BLM_L_UPPER:       /* plug for switch */      break;
+        }
+    else if (fmt->assign && !signing)
+        switch (fmt->lenmod) {
+            case BLM_NONE: *va_arg(args,      uint*) = result; break;
+            case BLM_HH  : *va_arg(args,     uchar*) = result; break;
+            case BLM_H   : *va_arg(args,    ushort*) = result; break;
+            case BLM_L   : *va_arg(args,     ulong*) = result; break;
+            case BLM_LL  : *va_arg(args,    ullong*) = result; break;
+            case BLM_J   : *va_arg(args, uintmax_t*) = result; break;
+            case BLM_Z   : *va_arg(args,    size_t*) = result; break;
+            case BLM_T   : *va_arg(args, ptrdiff_t*) = result; break;
+            case BLM_L_UPPER:       /* plug for switch */      break;
+        }
+
+    return B_OKEY;
 }
 
 int vbiscanf(BUFFER* buf, const char* fmt, va_list args) {
@@ -214,6 +314,13 @@ int vbiscanf(BUFFER* buf, const char* fmt, va_list args) {
                             total_count += 1;
                         }
                     } break;
+
+                    case 'i':
+                        if (fmt.lenmod == BLM_L_UPPER) goto error;
+                        if (fmt.maxwidth == 0) fmt.maxwidth = SIZE_MAX;
+                        if (bistrtouim(buf, &fmt, args, 0, &total_len, true)) goto error;
+                        if (fmt.assign) total_count += 1;
+                        break;
 
                     default: goto error;
                 }
