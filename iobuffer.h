ee5d88f928ae396a73338864ae3d0029913f1def
Stepainpy <stepa2014vlg@gmail.com>
2026-05-30 15:53:31 +0300

    update version

--- iobuffer.h
+++ iobuffer.h
@@ -18,7 +18,7 @@
 #define __btostr(x) __btostr_(x)
 
 #define IOBUFFER_VERSION_MAJOR 2
-#define IOBUFFER_VERSION_MINOR 2
+#define IOBUFFER_VERSION_MINOR 3
 #define IOBUFFER_VERSION_PATCH 0
 #define IOBUFFER_VERSION \
     __btostr(IOBUFFER_VERSION_MAJOR) "." \
