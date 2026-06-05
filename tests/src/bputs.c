19ce3c5b547a2a8dc803307a75d4ac16c6978de3
Stepainpy <stepa2014vlg@gmail.com>
2026-06-05 22:54:24 +0300

    add tests fot bputs

---
+++ tests/src/bputs.c
@@ -0,0 +1,42 @@
+#include <iobuffer/iobuffer.h>
+#include "test.h"
+
+int main(void) {
+    BUFFER* buf; BUFVIEW bvw;
+    char buffer[4];
+
+    TEST_CASE("call with null pointer", bputs(NULL, NULL) == EOB);
+
+    buf = bopen("Text", 4, "r");
+    TEST_CASE("call with not writable", bputs(NULL, buf) == EOB);
+    bclose(buf);
+
+    buf = bopen(NULL, 0, "w");
+    TEST_CASE("call with null data", bputs(NULL, buf) == EOB);
+    bclose(buf);
+
+    buf = bmemopen(buffer, sizeof buffer, "w");
+
+    TEST_CASE("put string", bputs("Tex", buf) >= 0);
+    TEST_CASE("put string", bputs("ts.", buf) <  0);
+
+    bvw = bview(buf);
+    TEST_CASE("after put", BV_LEN(bvw, base, stop) == 3);
+    TEST_CASE("after put", BV_LEN(bvw, base, head) == 3);
+    TEST_CASE("after put", BV_LEN(bvw, head, stop) == 0);
+    TEST_CASE("after put", memcmp(buffer, "Tex", 3) == 0);
+
+    bseek(buf, 1, BSEEK_SET);
+
+    TEST_CASE("put string", bputs("ort", buf) >= 0);
+    bvw = bview(buf);
+    TEST_CASE("after put", BV_LEN(bvw, base, stop) == 4);
+    TEST_CASE("after put", BV_LEN(bvw, base, head) == 4);
+    TEST_CASE("after put", BV_LEN(bvw, head, stop) == 0);
+    TEST_CASE("after put", memcmp(buffer, "Tort", 4) == 0);
+
+
+    bclose(buf);
+
+    return EXIT_SUCCESS;
+}
