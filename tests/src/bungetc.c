17c49303e651bfc12d92090a625589d58e7ca564
Stepainpy <stepa2014vlg@gmail.com>
2026-06-05 23:30:21 +0300

    add tests for bungets

---
+++ tests/src/bungetc.c
@@ -0,0 +1,42 @@
+#include <iobuffer/iobuffer.h>
+#include "test.h"
+
+int main(void) {
+    BUFFER* buf; BUFVIEW bvw;
+
+    TEST_CASE("call with null pointer", bungetc(0, NULL) == EOB);
+
+    buf = bopen(NULL, 0, "w");
+    TEST_CASE("call with not allocated data", bungetc(0, buf) == EOB);
+    bclose(buf);
+
+    buf = bopen("Text", 4, "a");
+    TEST_CASE("call with not readable", bungetc(0, buf) == EOB);
+    bclose(buf);
+
+    buf = bopen("Text", 4, "a+");
+
+    TEST_CASE("unget character", bungetc('m', buf) == 'm');
+    TEST_CASE("unget character", bungetc('r', buf) == 'r');
+
+    bvw = bview(buf);
+    TEST_CASE("check intermedian", BV_LEN(bvw, base, stop) == 4);
+    TEST_CASE("check intermedian", BV_LEN(bvw, base, head) == 2);
+    TEST_CASE("check intermedian", BV_LEN(bvw, head, stop) == 2);
+    TEST_CASE("check intermedian", memcmp(bvw.base, "Term", 4) == 0);
+
+    TEST_CASE("unget character", bungetc('a', buf) == 'a');
+    TEST_CASE("unget character", bungetc('H', buf) == 'H');
+    TEST_CASE("unget character", bungetc('>', buf) == EOB);
+    TEST_CASE("unget character", bungetc(EOB, buf) == EOB);
+
+    bvw = bview(buf);
+    TEST_CASE("check intermedian", BV_LEN(bvw, base, stop) == 4);
+    TEST_CASE("check intermedian", BV_LEN(bvw, base, head) == 0);
+    TEST_CASE("check intermedian", BV_LEN(bvw, head, stop) == 4);
+    TEST_CASE("check intermedian", memcmp(bvw.base, "Harm", 4) == 0);
+
+    bclose(buf);
+
+    return EXIT_SUCCESS;
+}
